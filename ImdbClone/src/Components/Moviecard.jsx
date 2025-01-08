9b48be0389068b68f4eba5077739b194c545f1fd
Abhishek <abhishekgoel@gofynd.com>
2025-01-08 23:24:17 +0530

    movieCard comp created

--- ImdbClone/src/Components/Moviecard.jsx
+++ ImdbClone/src/Components/Moviecard.jsx
@@ -5,7 +5,7 @@ function MovieCard({ movieObject }) {
     <div
       className="h-[40vh] w-[200px] bg-center bg-cover rounded-xl hover:scale-110 duration-300 hover:cursor-pointer flex flex-col justify-between items-end"
       style={{
-        backgroundImage: ``,
+        backgroundImage: `url('https://image.tmdb.org/t/p/w500/${movieObject.poster_path})`,
       }}
     >
       <div className="text-white w-full text-center text-xl p-2 bg-gray-900/70 rounded-lg">
@@ -14,14 +14,5 @@ function MovieCard({ movieObject }) {
     </div>
   );
 }
-
-<div
-              key={idx}
-              className="h-[40vh] w-[200px] bg-center bg-cover rounded-xl hover:scale-110 duration-300 hover:cursor-pointer flex flex-col justify-between item-end"
-              style={{ backgroundImage: `url('https://image.tmdb.org/t/p/w500/${movieObj.poster_path
-              })` }}
-            >
-              <div className="text-white w-full text-center text-xl bg-gray-900/70 p-2 rounded-lg">{movieObj.title}</div>
-            </div>
 
 export default MovieCard;
