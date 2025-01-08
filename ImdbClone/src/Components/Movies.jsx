13e011de759fc58dc8ba9d1f4df4cd48705742d5
Abhishek <abhishekgoel@gofynd.com>
2025-01-08 23:11:00 +0530

    banner

--- ImdbClone/src/Components/Movies.jsx
+++ ImdbClone/src/Components/Movies.jsx
@@ -68,7 +68,7 @@ function Movies() {
             <div
               key={idx}
               className="h-[40vh] w-[200px] bg-center bg-cover rounded-xl hover:scale-110 duration-300 hover:cursor-pointer flex flex-col justify-between item-end"
-              style={{ backgroundImage: `url('https://image.tmdb.org/t/p/w500/${movieObj.backdrop_path
+              style={{ backgroundImage: `url('https://image.tmdb.org/t/p/w500/${movieObj.poster_path
               })` }}
             >
               <div className="text-white w-full text-center text-xl bg-gray-900/70 p-2 rounded-lg">{movieObj.title}</div>
