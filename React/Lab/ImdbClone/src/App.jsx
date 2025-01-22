6ffab6e54efca05946481134a982af3e0b70f1d2
Abhishek <abhishekgoel@gofynd.com>
2025-01-22 22:31:28 +0530

    deploying

--- React/Lab/ImdbClone/src/App.jsx
+++ React/Lab/ImdbClone/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Components/Navbar.jsx'
 import Watchlist from './Components/Watchlist.jsx'
 import Home from './Components/Home'
 import { Route, Routes } from 'react-router-dom'
-import Context from './poc/Context'
-import ThemeManager from "./poc/themes/ThemeManager";
+// import Context from './poc/Context'
+// import ThemeManager from "./poc/themes/ThemeManager";
 import {Provider} from "react-redux"
 import store from './redux/store'
 function App() {
