8c293b398fbfeea345cd1df7953d048966a325f1
Abhishek <abhishekgoel@gofynd.com>
2025-01-27 22:33:13 +0530

    optimized

--- React/Classwork/reactperf/src/Components/Stopwatch.jsx
+++ React/Classwork/reactperf/src/Components/Stopwatch.jsx
@@ -1,39 +1,39 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect, useCallback } from 'react'
 
-function Stopwatch() {
+const Stopwatch = React.memo(() => {
     const [time,setTime] = useState(0);
     const [isRunning,setIsRunning] = useState(true);
     const intervalRef = useRef(null); //{current: null}
         useEffect(() => {
             intervalRef.current = setInterval(() => {
-                setTime(prevSec => prevSec+1000);
+                setTime(prevSec => prevSec+1);
             },1000);
         
         },[])
 
-    const startTimer = () =>{
+    const startTimer = useCallback(() =>{
         if(!isRunning){
             setIsRunning(true);
             intervalRef.current = setInterval(() => {
-                setTime(prevSec => prevSec+1000);
+                setTime(prevSec => prevSec+1);
             },1000);
         }
-    }
+    },[isRunning])
 
-    const stopTimer = () =>{
+    const stopTimer = useCallback(() =>{
         if(isRunning){
             setIsRunning(false);
             clearInterval(intervalRef.current)
         }
-    }
+    },[isRunning])
 
-    const resetTimer = () =>{
+    const resetTimer = useCallback(() =>{
         if(isRunning){
             setIsRunning(false);
             clearInterval(intervalRef.current)
             setTime(0);
         }
-    }
+    },[isRunning])
 
     const formatTime = (timeInSeconds) => {
         const getHours = `0${Math.floor(timeInSeconds/3600)}`.slice(-2);
@@ -52,6 +52,6 @@ function Stopwatch() {
         <buttoon onClick={resetTimer}>Reset</buttoon>
     </div>
   )
-}
+})
 
 export default Stopwatch
